5f10ea390218fd2fa4b0c0b20f1fe1862238ce76
zjx20 <zhoujianxiong2@gmail.com>
2017-07-08 18:00:07 +0800

    Fixed the consistency issue caused by hash collision
    Assume that two keys have the same hash value, the original
    implementation constructed the mapping table based on the order of the
    keys when adding into Map. It can cause inconsistent issues in a
    distributed environment.

--- consistenthash.go
+++ consistenthash.go
@@ -1,5 +1,6 @@
 /*
 Copyright 2013 Google Inc.
+Copyright 2017 zjx20
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -25,18 +26,21 @@ import (
 
 type Hash func(data []byte) uint32
 
+type node struct {
+	hash uint32
+	key  string
+}
+
 type Map struct {
 	hash     Hash
 	replicas int
-	keys     []int // Sorted
-	hashMap  map[int]string
+	nodes    []node // Sorted
 }
 
 func New(replicas int, fn Hash) *Map {
 	m := &Map{
 		replicas: replicas,
 		hash:     fn,
-		hashMap:  make(map[int]string),
 	}
 	if m.hash == nil {
 		m.hash = crc32.ChecksumIEEE
@@ -46,35 +50,34 @@ func New(replicas int, fn Hash) *Map {
 
 // Returns true if there are no items available.
 func (m *Map) IsEmpty() bool {
-	return len(m.keys) == 0
+	return len(m.nodes) == 0
 }
 
 // Adds some keys to the hash.
 func (m *Map) Add(keys ...string) {
 	for _, key := range keys {
 		for i := 0; i < m.replicas; i++ {
-			// log.Printf("adding %v, replica %v", key, i+1)
-			hash := int(m.hash([]byte(strconv.Itoa(i) + key)))
-			// log.Printf("hash = %v", hash)
-			m.keys = append(m.keys, hash)
-			m.hashMap[hash] = key
+			hash := m.hash([]byte(strconv.Itoa(i) + key))
+			m.nodes = append(m.nodes, node{hash: hash, key: key})
 		}
 	}
-	sort.Ints(m.keys)
+	sort.Slice(m.nodes, func(i, j int) bool {
+		if m.nodes[i].hash != m.nodes[j].hash {
+			return m.nodes[i].hash < m.nodes[j].hash
+		}
+		return m.nodes[i].key < m.nodes[j].key
+	})
 }
 
 // Remove a key from the hash.
 func (m *Map) Remove(key string) {
-	// log.Printf("removing %v ", key)
-	newKeys := []int{}
-	for _, v := range m.keys {
-		if m.hashMap[v] != key {
-			newKeys = append(newKeys, v)
-		} else {
-			delete(m.hashMap, v)
+	newNodes := make([]node, 0, len(m.nodes))
+	for _, n := range m.nodes {
+		if n.key != key {
+			newNodes = append(newNodes, n)
 		}
 	}
-	m.keys = newKeys
+	m.nodes = newNodes
 }
 
 // Gets the closest item in the hash to the provided key.
@@ -83,15 +86,17 @@ func (m *Map) Get(key string) string {
 		return ""
 	}
 
-	hash := int(m.hash([]byte(key)))
+	hash := m.hash([]byte(key))
 
 	// Binary search for appropriate replica.
-	idx := sort.Search(len(m.keys), func(i int) bool { return m.keys[i] >= hash })
+	idx := sort.Search(len(m.nodes), func(i int) bool {
+		return m.nodes[i].hash >= hash
+	})
 
 	// Means we have cycled back to the first replica.
-	if idx == len(m.keys) {
+	if idx == len(m.nodes) {
 		idx = 0
 	}
 
-	return m.hashMap[m.keys[idx]]
+	return m.nodes[idx].key
 }
--- consistenthash_test.go
+++ consistenthash_test.go
@@ -1,5 +1,6 @@
 /*
 Copyright 2013 Google Inc.
+Copyright 2017 zjx20
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -18,12 +19,12 @@ package consistenthash
 
 import (
 	"fmt"
+	"hash/crc32"
 	"strconv"
 	"testing"
 )
 
 func TestHashing(t *testing.T) {
-
 	// Override the hash function to return easier to reason about values. Assumes
 	// the keys can be converted to an integer.
 	hash := New(3, func(key []byte) uint32 {
@@ -76,6 +77,14 @@ func TestHashing(t *testing.T) {
 	}
 }
 
+func TestEmpty(t *testing.T) {
+	hash := New(1, nil)
+	v := hash.Get("foo")
+	if v != "" {
+		t.Errorf("Should return empty string")
+	}
+}
+
 func TestConsistency(t *testing.T) {
 	hash1 := New(1, nil)
 	hash2 := New(1, nil)
@@ -94,7 +103,36 @@ func TestConsistency(t *testing.T) {
 		hash1.Get("Bonny") != hash2.Get("Bonny") {
 		t.Errorf("Direct matches should always return the same entry")
 	}
+}
 
+func TestCollision(t *testing.T) {
+	v := "collision value"
+	words := map[string]struct{}{}
+	collisionHashFunc := func(data []byte) uint32 {
+		s := string(data)
+		if s == "2foo" || s == "0bar" {
+			words[s] = struct{}{}
+			// simulate hash collision
+			return crc32.ChecksumIEEE([]byte(v))
+		}
+		return crc32.ChecksumIEEE(data)
+	}
+
+	hash1 := New(3, collisionHashFunc)
+	hash2 := New(3, collisionHashFunc)
+
+	// The order of the keys should not affect the hash result.
+	hash1.Add("foo", "bar")
+	hash2.Add("bar", "foo")
+
+	// Make sure the test case is valid.
+	if len(words) != 2 {
+		t.Errorf("The test case doesn't match the current implementation.")
+	}
+
+	if hash1.Get(v) != hash2.Get(v) {
+		t.Errorf("Hashes should be the same")
+	}
 }
 
 func BenchmarkGet8(b *testing.B)   { benchmarkGet(b, 8) }
@@ -103,7 +141,6 @@ func BenchmarkGet128(b *testing.B) { benchmarkGet(b, 128) }
 func BenchmarkGet512(b *testing.B) { benchmarkGet(b, 512) }
 
 func benchmarkGet(b *testing.B, shards int) {
-
 	hash := New(50, nil)
 
 	var buckets []string
